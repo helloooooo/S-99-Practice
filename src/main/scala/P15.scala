13a6166b91c40ac4408723ebb266282d87f45e6b
NaokiSatou <yomicerisier@gmail.com>
2017-10-24 23:35:37 +0900

    4th

---
+++ src/main/scala/P15.scala
@@ -0,0 +1,13 @@
+class P15{
+  def duplicateN[T](n:Int,l:List[T]):List[T] = { l match {
+    case x :: xs => Nmap(x,n,Nil) ::: duplicateN(n,xs)
+    case Nil => Nil
+  }
+    def Nmap[T](c:T,n:Int,sub:List[T]):List[T] = { n match {
+      case 0 => sub
+      case n  => Nmap(c,n-1,c::sub)
+    }
+    }
+    
+  }
+}
